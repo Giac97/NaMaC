3740671d6d7077685eb010f7afcf3ce6c85dfaf6
Giacomo Becatti <giacomo.becatti@studenti.unimi.it>
2024-02-20 10:22:34 +0100

    Added subroutine for finding surfaces

--- src/surfacer.f90
+++ src/surfacer.f90
@@ -2,28 +2,31 @@ module surfacer
     use utilities
     implicit none
     private
-    public: surfacer
+    public  ::  find_surface
     !> @brief Determines whether an atom lies on the surface or is part of the bulk, returns as an array of 1s and 0s 
     !! depending on whether the i-th atoms is on the surface or not
     !!
     !! @param
-    subroutine surfacer(gcn, is_surface, gcn_cutoff)
-        real, dimension(:), intent(in)    ::  gcn
-        real,intent(in9                     ::  gcn_cutoff
-        real, allocatable(:), intent(out)   ::  is_surface
+contains
+    subroutine find_surface(gcn, is_surface, gcn_cutoff)
+        real, dimension(:), intent(in)          ::  gcn
+        real,intent(in)                         ::  gcn_cutoff
+        integer, allocatable, intent(out)    ::  is_surface(:)
         
         integer     ::  i, j, N_atoms
         
         N_atoms = size(gcn)
+        allocate(is_surface(N_atoms))
         !$ACC KERNELS
         !$ACC LOOP INDEPENDENT
         do i = 1, N_atoms
-            if (gcn.le.gcn_cutoff) is_surface(i) = 1
-            else
+            if (gcn(i).le.gcn_cutoff) then
+                is_surface(i) = 1
+            else 
                 is_surface(i) = 0
             endif
 
         enddo
-        
-    end subroutine surfacer
+        !$ACC END KERNELS        
+    end subroutine find_surface
 end module surfacer
