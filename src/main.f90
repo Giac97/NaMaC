1a3bf6bf5e3c8df53cb0c6782912ccbca91445d6
Giacomo Becatti <giacomo.becatti@studenti.unimi.it>
2024-02-20 16:37:29 +0100

    Added functionality to read from input file

--- src/main.f90
+++ src/main.f90
@@ -5,15 +5,25 @@ program namac
     implicit none
     real, allocatable       ::  coordinates(:,:), gcn(:)
     integer, allocatable    ::  coordination(:), neigh_list(:,:), is_surface(:)
-    real                    ::  r_atoms = 3.5, r_probe = 0.0
-    integer                 ::  n_samples = 300000, acc
-    real                    ::  pore
+    real                    ::  r_atom , r_probe 
+    integer                 ::  n_samples, acc, pbc
+    real                    ::  pore, CNmax, cutoff, gcn_cutoff
     real, allocatable       ::  insert(:,:)
 
 
     character(len=20)   ::  fname
     character(len=50)   ::  outname, gcname, surfname, porname
+
+
     fname = "large.xyz"
+    namelist/parameters/fname,cutoff,CNmax,gcn_cutoff,pbc
+    namelist/porosity/r_atom, r_probe, n_samples
+    ! Call the subroutine to read coordinates from a file
+    
+    open(25, file="input.in", status="old", action="read")
+    read(25, parameters)
+    read(25, porosity)
+    close(25)
     outname = "out_cord.xyz"
     gcname = "out_gcn.xyz"
     surfname = "out_surf.xyz"
@@ -27,7 +37,7 @@ program namac
     
     write(*,*) "Now for the lengthiest part so far, the CN, this will take a while"
     write(*,*)
-    call coordination_calc(coordinates, 3.5, 0, coordination, neigh_list)
+    call coordination_calc(coordinates, cutoff, pbc, coordination, neigh_list)
     write(*,*) "CN and neighbor list computed"
 
     write(*,*) "Now the gcn"
@@ -37,14 +47,14 @@ program namac
     write(*,*) "Outputting all files"
     call write_xyz_coordination(outname, coordinates, coordination)
     call write_xyz_gcn(gcname, coordinates, gcn)
-    call find_surface(gcn, is_surface, 10.0)
+    call find_surface(gcn, is_surface, gcn_cutoff)
     call write_xyz_coordination(surfname, coordinates, is_surface) 
     
     write(*,*) "You thought the cn took a long time?"
     write(*,*) "Well, if you do not have a CUDA GPU then you'll be right"
     write(*,*) "Now here is the longest calculation, this bad boy can not be parallelized due to some exit clauses"
     write(*,*) "Computing the porosity"
-    call find_porosity(coordinates, r_atoms, r_probe, n_samples, pore, insert, acc)
+    call find_porosity(coordinates, r_atom, r_probe, n_samples, pore, insert, acc)
     write(*,*) "Porosity = ", pore
     call write_xyz_porosity(porname, coordinates, insert, acc)
     deallocate(coordination, coordinates, neigh_list, gcn)
