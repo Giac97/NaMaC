59958a055d36189ccd3078f92b9288539a7d9357
Giacomo Becatti <giacomo.becatti@studenti.unimi.it>
2024-02-20 15:53:16 +0100

    Added saving porosity xyz and output porosity every 1000 samples

--- src/main.f90
+++ src/main.f90
@@ -1,26 +1,52 @@
 program namac
     use utilities
     use surfacer
+    use porosity
     implicit none
     real, allocatable       ::  coordinates(:,:), gcn(:)
     integer, allocatable    ::  coordination(:), neigh_list(:,:), is_surface(:)
-      
+    real                    ::  r_atoms = 3.5, r_probe = 0.0
+    integer                 ::  n_samples = 300000, acc
+    real                    ::  pore
+    real, allocatable       ::  insert(:,:)
+
 
     character(len=20)   ::  fname
-    character(len=50)   ::  outname, gcname, surfname
-    fname = "test.xyz"
+    character(len=50)   ::  outname, gcname, surfname, porname
+    fname = "large.xyz"
     outname = "out_cord.xyz"
     gcname = "out_gcn.xyz"
     surfname = "out_surf.xyz"
-    
+    porname = "out_pore.xyz"
+    write(*,*) "Welcome to the main NaMaC loop"
+    write(*,*) "We will begin by loading the xyz file you want to analyse"
+    write(*,*) ""
     call read_xyz(fname, coordinates)
+    write(*,*) "Loaded ", size(coordinates, 2), " atoms"
     allocate(coordination(size(coordinates,2)), neigh_list(size(coordinates,2),12))
+    
+    write(*,*) "Now for the lengthiest part so far, the CN, this will take a while"
+    write(*,*)
     call coordination_calc(coordinates, 3.5, 0, coordination, neigh_list)
-    call  gcn_calc(coordination, neigh_list, 12.0, gcn)
+    write(*,*) "CN and neighbor list computed"
 
+    write(*,*) "Now the gcn"
+    call  gcn_calc(coordination, neigh_list, 12.0, gcn)
+    write(*,*) "GCN computed"
+
+    write(*,*) "Outputting all files"
     call write_xyz_coordination(outname, coordinates, coordination)
     call write_xyz_gcn(gcname, coordinates, gcn)
     call find_surface(gcn, is_surface, 10.0)
     call write_xyz_coordination(surfname, coordinates, is_surface) 
+    
+    write(*,*) "You thought the cn took a long time?"
+    write(*,*) "Well, if you do not have a CUDA GPU then you'll be right"
+    write(*,*) "Now here is the longest calculation, this bad boy can not be parallelized due to some exit clauses"
+    write(*,*) "Computing the porosity"
+    call find_porosity(coordinates, r_atoms, r_probe, n_samples, pore, insert, acc)
+    write(*,*) "Porosity = ", pore
+    call write_xyz_porosity(porname, coordinates, insert, acc)
     deallocate(coordination, coordinates, neigh_list, gcn)
+    write(*,*) "Seems like we are done, hopefully nothing crashed along the way"
 end program namac
---
+++ src/porosity.f90
@@ -0,0 +1,97 @@
+module porosity
+    use utilities
+    implicit none
+    private
+    public  ::  find_porosity, write_xyz_porosity
+contains
+
+    subroutine find_porosity(coordinates, r_atoms, r_probe, n_samples, pore, insert, acc)
+        implicit none
+        real, intent(in)        ::  coordinates(:,:)
+        real, intent(in)        ::  r_atoms, r_probe
+        integer, intent(in)     ::  n_samples
+        
+        real, intent(out)       ::  pore
+        real, intent(out), allocatable  ::  insert(:,:)
+        integer, intent(out)    ::  acc
+        real, allocatable       ::  insert_temp(:,:)
+        real                    ::  test_insert(3)
+        
+        real                    ::  x_min, x_max 
+        real                    ::  y_min, y_max
+        real                    ::  z_min, z_max
+        
+        real                    ::  dist
+
+        integer                 ::  i, j, N_atoms, rej, seed, idx
+
+        N_atoms = size(coordinates, 2)
+        
+        x_min = minval(coordinates(1,:))
+        x_max = maxval(coordinates(1,:))
+        
+        y_min = minval(coordinates(2,:))
+        y_max = maxval(coordinates(2,:))
+        
+        z_min = minval(coordinates(3,:))
+        z_max = maxval(coordinates(3,:))
+        allocate(insert(3,n_samples))
+        
+        acc = 0
+        seed = 176 
+        idx = 0
+        call random_seed()
+        do i = 1, n_samples
+            rej = 0
+            call random_number(test_insert)
+            test_insert(1) = (x_max - x_min) * test_insert(1) + x_min
+            test_insert(2) = (y_max - y_min) * test_insert(2) + y_min
+            test_insert(3) = (z_max - z_min) * test_insert(3) + z_min
+            do j = 1, N_atoms
+                call distance(test_insert, coordinates(:,j), dist)
+                if (dist - (r_atoms + r_probe) .lt. 0) then
+                    rej = 1
+                    exit 
+                endif
+            enddo
+            
+            if (rej .eq. 0) then
+                acc = acc + 1
+                insert(:,i) = test_insert
+                idx = idx + 1
+            endif
+            
+            if (mod(i,1000).eq.0) then
+                write(*,*) "Sample ", i, " of ", n_samples, ", porosity= ", real(acc) / real(i)
+            endif
+        enddo
+
+        pore = real(acc) / real(n_samples)
+    end subroutine find_porosity
+
+    subroutine write_xyz_porosity(fname, coordinates, insert, acc)
+        implicit none
+        character(len=50), intent(in)   ::  fname
+        real, intent(in)                ::  coordinates(:,:), insert(:,:)
+        integer, intent(in)             ::  acc
+        integer                         ::  N_atoms,  i, N_samples
+
+        N_atoms = size(coordinates, 2)
+        N_samples = size(insert, 2)
+        open(10, file=fname, status="replace", action="write")
+        write(10, *) N_atoms + acc
+        write(10, *) "# Writing porosity and atoms to xyz"
+        do i = 1, N_atoms
+            write(10, '(A2, 3F15.10)') "Au", coordinates(:,i)
+        enddo
+
+        do i = 1, N_samples
+            if (abs(insert(1,i)) .ge. 0.0003 .and. abs(insert(2,i)) .ge. 0.0002 .and. abs(insert(3,i)) .ge. 0.0002) then
+                write(10, '(A2, 3F15.10)') "XX", insert(:,i)
+            endif
+        enddo
+
+        close(10)
+            
+    end subroutine
+end module porosity
